bfb70af60942d9cf2b129d3d45153b8df847a6a2
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-04-06 12:11:02 -0300

    🤡 mock: add products mock

--- src/mocks/products.ts
+++ src/mocks/products.ts
@@ -3,37 +3,37 @@ export const products = [
     _id: '6372e040f52e37ef85fe2c5e',
     name: 'Pizza quatro queijos',
     description: 'Deliciosa pizza quatro queijos com borda simples',
-    imagePath: '1668472896991-quatro-queijos.png',
+    imagePath: 'dbaef046-3ea5-4df0-937b-4f71b3a662ae-quatro-queijos.png',
     price: 40,
     ingredients: [
       {
         name: 'Mussarela',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c5f'
+        _id: '6372e040f52e37ef85fe2c5f',
       },
       {
         name: 'Parmesão',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c60'
+        _id: '6372e040f52e37ef85fe2c60',
       },
       {
         name: 'Gouda',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c61'
+        _id: '6372e040f52e37ef85fe2c61',
       },
       {
         name: 'Brie',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c62'
-      }
-    ]
+        _id: '6372e040f52e37ef85fe2c62',
+      },
+    ],
   },
   {
     _id: '6372e276a381106c0f854cb3',
     name: 'Coca cola',
     description: 'Coca cola lata geladinha topzera',
-    imagePath: '1668473462705-coca-cola.png',
+    imagePath: '9cce94be-081c-4bad-910a-c3f270af8c10-coca-cola.png',
     price: 7,
-    ingredients: []
-  }
+    ingredients: [],
+  },
 ];
