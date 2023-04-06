2afd478f56072357eb16d4b9f7e15642cc83fecc
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-04-06 12:13:34 -0300

    ✨ feat: join all components

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,10 +1,75 @@
+import { useState } from 'react';
+import { Categories } from '../components/Categories';
+import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
+import { Menu } from '../components/Menu';
+import { TableModal } from '../components/TableModal';
 import { Container } from './styles';
 
+export type Product = {
+  _id: string;
+  name: string;
+  description: string;
+  imagePath: string;
+  price: number;
+  ingredients: {
+    name: string;
+    icon: string;
+    _id: string;
+  }[];
+};
+
 export const Main = () => {
+  const [activeCategory, setActiveCategory] = useState<string | null>(null);
+  const [productsInCar, setProductsInCar] = useState<Product[]>([]);
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [order, setOrder] = useState<null | Record<string, string>>(null);
+
+  const handleSelectCategory = (id: string) => () => {
+    setActiveCategory((prev) => (prev === id ? null : id));
+  };
+
+  const handleSetProductsInCar = (product: Product) => {
+    const idx = productsInCar.findIndex((p) => p._id === product._id);
+
+    if (idx !== -1) {
+      setProductsInCar(productsInCar.filter((p) => p._id !== product._id));
+    } else {
+      setProductsInCar((prev) => [...prev, product]);
+    }
+  };
+
+  const handleShowTableModal = () => {
+    setIsTableModalVisible((prev) => !prev);
+  };
+
+  const handleSaveTable = (table: string) => {
+    setOrder({ table });
+    alert('Pedido realizado com sucesso!');
+  };
+
+  const handleCloseModal = () => {
+    setIsTableModalVisible(false);
+  };
+
   return (
     <Container>
-      <Header/>
+      <Header selectedOrder={order} />
+
+      <Categories
+        onSelectActiveCategory={handleSelectCategory}
+        activeCategory={activeCategory}
+      />
+
+      <Menu setProductsInCar={handleSetProductsInCar} />
+
+      <Footer showTableModal={handleShowTableModal} selectedOrder={order} />
+
+      <TableModal
+        closeModal={handleCloseModal}
+        visible={isTableModalVisible}
+        onSave={handleSaveTable}
+      />
     </Container>
   );
 };
--- src/main/styles.ts
+++ src/main/styles.ts
@@ -1,13 +1,8 @@
-import { Platform, StatusBar } from 'react-native';
 import styled from 'styled-components/native';
 
-const isAndroid = Platform.OS === 'android';
-
-console.log({
-  isAndroid,
-  StatusBarHeight: StatusBar.currentHeight,
-});
-
 export const Container = styled.SafeAreaView`
+  flex: 1;
   padding: 24px;
+  padding-bottom: 0;
+  background: #fafafa;
 `;
