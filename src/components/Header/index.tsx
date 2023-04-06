a0ce7db083a1099f8754db04840c350b3d30fd54
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-04-06 12:13:08 -0300

    ♻️  refactor: refactor Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,29 @@
 import { Text } from '../Text';
 import { Container } from './styles';
 
-export const Header = () => {
+interface HeaderProps {
+  selectedOrder: null | Record<string, string>;
+}
+
+export const Header = ({ selectedOrder }: HeaderProps) => {
   return (
     <Container>
-      <Text size={14} color="#333" opacity={0.9}>
-        Bem vindo(a) ao
-      </Text>
+      {!selectedOrder ? (
+        <>
+          <Text size={14} color="#333" opacity={0.9}>
+            Bem vindo(a) ao
+          </Text>
 
-      <Text size={24} weight="700">
-        RAPID
-        <Text size={24}>
-          SERVE
+          <Text size={24} weight="700">
+            RAPID
+            <Text size={24}>SERVE</Text>
+          </Text>
+        </>
+      ) : (
+        <Text size={24} weight="700">
+          {selectedOrder.table}
         </Text>
-      </Text>
+      )}
     </Container>
   );
 };
